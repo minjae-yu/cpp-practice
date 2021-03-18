eb13013a5a302ea95dfebc748ff1fbebad0183a7
minjae-yu <minjae.yoo@digipen.edu>
2021-03-19 01:38:44 +0900

    added cpp

---
+++ practice/practice/conditionalstatements(hackerrank).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string num
+
+    int n;
+    std::cin >> n;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // Write Your Code Here
+
+        return 0;
+}
